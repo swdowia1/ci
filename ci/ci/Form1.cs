24faaad9c06d3c6f525d31bd091761ea2c0cacd6
testswzaqwsx@gmail.com <testswzaqwsx@gmail.com>
2020-02-17 11:07:07 +0100

    first commit

---
+++ ci/ci/Form1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace ci
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("zmiany sobotnie");
+            //jakis zmiana
+        }
+    }
+}
